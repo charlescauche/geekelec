2763618b8a7f2d04621941daeeff2e7cc4104814
Charles Cauche <charles.cauche@gmail.com>
2019-02-09 18:13:20 +0100

    Add initial local files to gitlab

---
+++ mailSub.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3.7.2
+# -*-coding:Utf-8 -*
+
+"""function mailSub to register user email in a list"""
+
+import sys
+print(sys.version)
+
+
+def addMail():
+	"""Add the user mail to the list emails"""
+	mailing_list=[]
+	user_mail = input("Please enter your email: ") # User give his email address
+	
+	if not "@" in user_mail: #Test to verify if this is a valid email address
+		print("You didn't enter a valid email address")
+
+	else: #if email input is valid
+		mailing_list.append(user_mail)
+		print("Thanks, we added your address to our list")
+		print("We will send you an email to validate your subsciption")
+		print(mailing_list)
+
+
+
+
+# Version test 
+if __name__ == "__main__":
+	print(sys.version)
+	print("TEST FONCTION addMail")
+	addMail()
+

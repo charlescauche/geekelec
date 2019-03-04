320ebce377d1f32e2e3035df3daa60e27fbae005
Charles Cauche <charles.cauche@gmail.com>
2019-03-05 00:10:52 +0100

    printing forcasts in a readable format and only for D-1 values and specific production type

--- geekelec.py
+++ geekelec.py
@@ -4,16 +4,12 @@ import json
 import secret
 
 
-
-# base = "YWY1NmZiYzctZTc1OS00NDMwLTkzYzMtYjk5NTRlYTgxYmNiOjJjNTliZTMxLWU2MWMtNDdjMS1iZmI1LWM4YTU1ZGRlZDhiYw=="
-
 #Autorization step
 
 URL = 'https://digital.iservices.rte-france.com/token/oauth/'
-headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization' : 'Basic '+secret.token}
+headers = {'Authorization' : 'Basic '+secret.token}
 
 r = requests.post(URL, headers=headers)
-
 print(r)
 
 token = r.json()['access_token']
@@ -21,20 +17,35 @@ token = r.json()['access_token']
 
 #API access
 
-#sbURL='https://digital.iservices.rte- france.com/open_api/generation_forecast/v2/sandbox/forecasts'
+# sandbox URL='https://digital.iservices.rte- france.com/open_api/generation_forecast/v2/sandbox/forecasts'
 rURL='https://digital.iservices.rte-france.com/open_api/generation_forecast/v2/forecasts'
 headers = {'Authorization':'Bearer '+token}
 
-# print('=====================')
-# print(datetime.date.today())
-# print('=====================')
 
-payload = {'start_date' : '2019-03-04T00:00:00Z' , 'end_date' : '2019-03-06T00:00:00Z' }
+#payload = {'start_date' : '2019-03-04T00:00:00Z' , 'end_date' : '2019-03-06T00:00:00Z' }
+
+payload = {'type' : 'D-1' , 'production_type' : 'AGGREGATED_FRANCE,WIND,SOLAR' }
 
 
 r = requests.get(rURL, params=payload, headers=headers)
 
-print("===========")
 p = json.dumps(r.json(),indent = 4)
+print("===========print(p)")
 print(p)
+print("===========")
+
+print()
+
+print('--------------------------')
+i = 0
+for l in r.json()['forecasts']:
+	print('Element ',i)
+	print(l['start_date'])
+	print(l['end_date'])
+	print(l['production_type'])
+	print('--------------------------')
+	i+=1
+
+for k,v in r.headers.items():
+	print(k,":",v)
 
